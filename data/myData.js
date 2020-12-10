0b0b937e2ba1afe7de753fd4a7beb0ac71d4ed76
craig <craig.edney@hotmail.com>
2020-12-10 19:51:21 +0000

    Create PUT endpoint to Update data

--- data/myData.js
+++ data/myData.js
@@ -59,6 +59,28 @@ let myData = {
                 });
             }
         });
+    },
+    update: function(newData, id, resolve, reject) {
+        fs.readFile(FILE_NAME, function(err, data) {
+            if(err){
+                reject(err);
+            }
+            else {
+                let myDatas = JSON.parse(data);
+                let myData = myDatas.find(d => d.id == id);
+                if (myData) {
+                    Object.assign(myData, newData);
+                    fs.writeFile(FILE_NAME, JSON.stringify(myDatas), function (err) {
+                        if(err){
+                            reject(err);
+                        }
+                        else {
+                            resolve(newData);
+                        }
+                    });
+                }
+            }
+        });
     }
 };
 
--- index.js
+++ index.js
@@ -42,7 +42,6 @@ router.get('/search', function (req, res, next) {
     });
 });
 
-
 // Create GET/id to return a single piece of data
 router.get('/:id', function (req, res, next) {
     myData.getById(req.params.id, function (data) {
@@ -70,6 +69,7 @@ router.get('/:id', function (req, res, next) {
         });
 });
 
+// Create POST in raw JSON to add data
 router.post('/', function (req, res, next) {
     myData.insert(req.body, function (data) {
         res.status(201).json({
@@ -78,6 +78,35 @@ router.post('/', function (req, res, next) {
             "message": "New data added",
             "data": data
         });
+    }, function (err) {
+        next(err);
+    });
+});
+
+// Create PUT to update a single piece of data
+router.put('/:id', function (req, res, next) {
+    myData.getById(req.params.id, function (data) {
+        if (data) {
+            // Attempt to update the data
+            myData.update(req.body, req.params.id, function (data) {
+                res.status(200).json({
+                    "status": 200,
+                    "statusText": "OK",
+                    "message": "Data '" + req.params.id + "' updated",
+                    "data": data
+                });
+            });
+        } else {
+            res.status(404).json({
+                "status": 404,
+                "statusText": "Not Found",
+                "message": "Data '" + req.params.id + "' could not be found",
+                "error": {
+                    "code": "NOT_FOUND",
+                    "message": "The data '" + req.params.id + "'could not be found"
+                }
+            });
+        }
     }, function (err) {
         next(err);
     });
