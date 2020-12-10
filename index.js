eaae387b858e109a3343c11b11e742205d95d9c1
craig <craig.edney@hotmail.com>
2020-12-10 20:22:02 +0000

    Create PATCH endpoint to Patch data

--- index.js
+++ index.js
@@ -141,6 +141,34 @@ router.delete('/:id', function (req, res, next) {
     });
 });
 
+router.patch('/:id', function (req, res, next) {
+        myData.getById(req.params.id, function (data) {
+            if (data) {
+                // Attempt to update the data
+                myData.update(req.body, req.params.id, function (data) {
+                    res.status(200).json({
+                        "status": 200,
+                        "statusText": "OK",
+                        "message": "Data '" + req.params.id + "' patched",
+                        "data": data
+                    });
+                });
+            } else {
+                res.status(404).json({
+                    "status": 404,
+                    "statusText": "Not Found",
+                    "message": "Data '" + req.params.id + "' could not be found",
+                    "error": {
+                        "code": "NOT_FOUND",
+                        "message": "The data '" + req.params.id + "'could not be found"
+                    }
+                });
+            }
+        }, function (err) {
+            next(err);
+        });
+});
+
 // Configure router so all routes are prefixed with /api/v1
 app.use('/api/', router);
 
