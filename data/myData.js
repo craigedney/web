c3dfacbc33b3f63a5e2ee3a0c2ade2bb00534ce2
craig <craig.edney@hotmail.com>
2020-12-10 18:25:05 +0000

    Get a single piece of data

--- data/myData.js
+++ data/myData.js
@@ -11,6 +11,17 @@ let myData = {
                 resolve(JSON.parse(data));
             }
         });
+    },
+    getById: function(id, resolve, reject){
+        fs.readFile(FILE_NAME, function(err, data){
+            if(err){
+                reject(err);
+            }
+            else {
+                let myData = JSON.parse(data).find(d => d.id == id);
+                resolve(myData);
+            }
+        });
     }
 };
 
--- index.js
+++ index.js
@@ -15,10 +15,38 @@ router.get('/', function (req, res, next) {
             "message": "All data retrieved",
             "data": data
         });
-    }, function(err) {
+    }, function (err) {
         next(err);
     });
 });
+
+// Create GET/id to return a single piece of data
+router.get('/:id', function (req, res, next) {
+    myData.getById(req.params.id, function (data) {
+            if (data) {
+                res.status(200).json({
+                    "status": 200,
+                    "statusText": "OK",
+                    "message": "Single piece of data retrieved",
+                    "data": data
+                });
+            } else {
+                res.status(404).json({
+                    "status": 404,
+                    "statusText": "Not Found",
+                    "message": "The data '" + req.params.id + "'could not be found",
+                    "error": {
+                        "code": "NOT_FOUND",
+                        "message": "The data '" + req.params.id + "'could not be found"
+                    }
+                });
+            }
+        },
+        function (err) {
+            next(err);
+        });
+});
+
 
 // Configure router so all routes are prefixed with /api/v1
 app.use('/api/', router);
